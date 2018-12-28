de6c3f4829287fce71a84c9687569fea4b4e7abd
Quan Hua <quanhua92@gmail.com>
2018-12-28 17:19:56 +0700

    update doctest in binary_search.py

--- algorithms-toolbox/search/binary_search.py
+++ algorithms-toolbox/search/binary_search.py
@@ -4,11 +4,11 @@ def binary_search(query, array):
     Determine whether the query is in an sorted array.
     Return the index of the query if it is present in the array.
     If the query is not in the array, return -1
-    >>> binary_search(4, [1, 2, 3, 4, 5, 6, 7, 8])
+    >>> binary_search(4, [1, 2, 3, 4, 5, 6, 7, 8, 9])
     3
-    >>> binary_search(8, [1, 2, 3, 4, 5, 6, 7, 8])
+    >>> binary_search(8, [1, 2, 3, 4, 5, 6, 7, 8, 9])
     7
-    >>> binary_search(10, [1, 2, 3, 4, 5, 6, 7, 8])
+    >>> binary_search(10, [1, 2, 3, 4, 5, 6, 7, 8, 9])
     -1
     """
     lo, hi = 0, len(array) - 1
